b40ae7120175f8bfcaf53a4ac6e92474d4345d73
Erielcio Imamura <erielcio00@gmail.com>
2021-11-10 20:28:44 -0300

    fix func

--- azure-function/python/GetBusData/__init__.py
+++ azure-function/python/GetBusData/__init__.py
@@ -11,25 +11,31 @@ AZURE_CONN_STRING = str(os.environ["AzureSQLConnectionString"])
 GTFS_RT_FEED = str(os.environ["RealTimeFeedUrl"])
 LOGIC_APP_URL = str(os.environ["LogicAppUrl"])
 
+
 def main(GetBusData: func.TimerRequest) -> func.HttpResponse:
-    ## Get the routes we want to monitor
+    logging.info('GetBusData function triggered !!!=' + AZURE_CONN_STRING)
+
+    # Get the routes we want to monitor
     routes = GetMonitoredRoutes()
-    
-    ## Get the real-time bus location feed
+
+    # Get the real-time bus location feed
     feed = GetRealTimeFeed()
-    
-    ## Filter only the routes we want to monitor
+
+    # Filter only the routes we want to monitor
     buses = [f for f in feed if int(f["RouteId"]) in routes]
 
-    logging.info('Received {0} buses positions, found {1} buses in monitored routes'.format(len(feed), len(buses)))
+    logging.info('Received {0} buses positions, found {1} buses in monitored routes'.format(
+        len(feed), len(buses)))
 
-    ## Push data to Azure SQL and get the activated geofences
+    # Push data to Azure SQL and get the activated geofences
     activatedGeofences = ProcessGeoFences(buses)
 
-    ## Send notifications 
+    # Send notifications
     for gf in activatedGeofences:
-        logging.info('Vehicle %i, route %s, %sing GeoFence %s at %s UTC', gf["VehicleId"], gf["RouteId"], gf["GeoFenceStatus"], gf["GeoFence"], gf["TimestampUTC"])
+        logging.info('Vehicle %i, route %s, %sing GeoFence %s at %s UTC',
+                     gf["VehicleId"], gf["RouteId"], gf["GeoFenceStatus"], gf["GeoFence"], gf["TimestampUTC"])
         TriggerLogicApp(gf)
+
 
 def GetRealTimeFeed():
     response = requests.get(GTFS_RT_FEED)
@@ -47,56 +53,63 @@ def GetRealTimeFeed():
             },
             "TimestampUTC": dt.utcfromtimestamp(v['timestamp']).isoformat(sep=' ')
         }
-        busData.append(busDetails)    
+        busData.append(busDetails)
     return busData
 
+
 def GetMonitoredRoutes():
-    result = executeQueryJSON('web.GetMonitoredRoutes')    
+    result = executeQueryJSON('web.GetMonitoredRoutes')
     return [r["RouteId"] for r in result]
 
-def ProcessGeoFences(payload):    
+
+def ProcessGeoFences(payload):
     result = {}
     if payload:
         result = executeQueryJSON('web.AddBusData', payload)
-        logging.info('Found %i buses activating a geofence',len(result))
+        logging.info('Found %i buses activating a geofence', len(result))
     return result
+
 
 def TriggerLogicApp(geoFence):
     content = {
-        "value1": str(geoFence["VehicleId"]), 
+        "value1": str(geoFence["VehicleId"]),
         "value2": str(geoFence["GeoFenceStatus"])
     }
 
-    logging.info("Calling Logic App webhook for {0}".format(geoFence["VehicleId"]))
+    logging.info("Calling Logic App webhook for {0}".format(
+        geoFence["VehicleId"]))
 
-    params = { 
-        "Content-type": "application/json" 
+    params = {
+        "Content-type": "application/json"
     }
 
     response = requests.post(LOGIC_APP_URL, json=content, headers=params)
     if response.status_code != 202:
-        logging.info('Error calling Logic App: {0}'.format(response.status_code))        
+        logging.info('Error calling Logic App: {0}'.format(
+            response.status_code))
     else:
-        logging.info("[%i/%i/%i] WebHook called successfully", geoFence["VehicleId"], geoFence["DirectionId"], geoFence["GeoFenceId"])
+        logging.info("[%i/%i/%i] WebHook called successfully",
+                     geoFence["VehicleId"], geoFence["DirectionId"], geoFence["GeoFenceId"])
+
 
 def executeQueryJSON(procedure, payload=None):
     result = {}
-    try: 
+    try:
         conn = pyodbc.connect(AZURE_CONN_STRING)
-        
+
         with conn.cursor() as cursor:
-            if payload:            
+            if payload:
                 cursor.execute(f"EXEC {procedure} ?", json.dumps(payload))
             else:
                 cursor.execute(f"EXEC {procedure}")
 
             result = cursor.fetchone()[0]
-            
+
             if result:
-                result = json.loads(result)                           
+                result = json.loads(result)
             else:
-                result = {}        
-        
+                result = {}
+
     finally:
         pass
 
